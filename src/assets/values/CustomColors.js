bdb46749eef2ddf9d2547a0b4bbb8519da45dcc1
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-09-24 23:27:26 +0530

    Added Custom Colors

--- src/assets/values/CustomColors.js
+++ src/assets/values/CustomColors.js
@@ -1,8 +1,37 @@
 export const CustomColors = {
     primaryColor: '#F5C518', // 'rgba(245, 197, 24,1)',
-    secondaryColor: '#000',
-    textColor: '#fff',
-    gray:'#eee',
     white: '#fff',
     black: '#000',
+};
+
+export const CustomDarkColors = {
+    
+    //Fix Colors
+    primaryColor: '#F5C518',
+    textColor: '#127BAB',
+    statusbarColor:'#1d1d1d',
+    plusIconBackgroundColor:'#686868',
+
+    //Change Colors
+    mainBackgroundColor:'#191919',
+    componentsBackgroundColor: '#212121',
+    inActiveIconColor: '#9E9E9E',
+    activeIconColor: '#F5C518',
+    bottomBarColor:'#1D1D1D',
+};
+
+export const CustomLightColors = {
+
+    //Fix Colors
+    primaryColor: '#F5C518',
+    textColor: '#127BAB',
+    statusbarColor:'#1d1d1d',
+    plusIconBackgroundColor:'#686868',
+    
+    //Change Colors
+    mainBackgroundColor: '#EEEEEE',
+    componentsBackgroundColor: '#FAFAFA',
+    inActiveIconColor: '#666666',
+    activeIconColor: '#000000',
+    bottomBarColor:'#FFFFFF',
 };
