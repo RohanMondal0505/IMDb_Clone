e8a3270d43ad08f47de666bd3792635558b03246
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-09-24 20:04:48 +0530

    Fix RootNavigation

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,11 +1,27 @@
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {useEffect} from 'react';
+import { useColorScheme } from 'react-native';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import SearchScreen from '../screens/SearchScreen';
+import VideosScreen from '../screens/VideosScreen';
+
+
+
 
 const Tab = createMaterialBottomTabNavigator();
 
 const RootNavigation = () => {
+    const colorScheme = useColorScheme();
+
+    useEffect(() => {
+        console.log(colorScheme)
+    }, [colorScheme]);
+
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator >
                 <Tab.Screen name="Home" component={HomeScreen} />
                 <Tab.Screen name="Search" component={SearchScreen} />
                 <Tab.Screen name="Videos" component={VideosScreen} />
