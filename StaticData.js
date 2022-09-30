87c33402fa67fc1d7d7d0c363507325287505e65
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-10-01 02:23:33 +0530

    Create a Static Data Folder

---
+++ StaticData.js
@@ -0,0 +1,156 @@
+export const slideData = [
+    'https://m.media-amazon.com/images/M/MV5BN2RjZDJhYzUtOTQ5Yy00OWM3LWE5OTctM2Y0YWVmNzAzODllXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg',
+    'https://gutsyindians.com/wp-content/uploads/2022/03/FOw6RHHaQAEJopz-1-1170x1657.jpg',
+    'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSCmwbcVs6Qbwf_vSX04HN4uGW7IwamqXrb8o0HIVWvUbuTTLt2',
+    'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQEJ6XEfDIp0CAlNbhoICrQjgwfdN14rYu19GVnL3NroFfsimr7',
+    'https://m.media-amazon.com/images/M/MV5BNmNmYjBmYjgtZjlmZC00OWQyLTgzZGQtMGVkNDQ5NDlkZTM2XkEyXkFqcGdeQXVyMTA5MjQ4MDg4._V1_.jpg',
+    'https://m.media-amazon.com/images/M/MV5BZjdjOTQwY2UtZDM5Zi00YTU0LWIzOTQtMTFjZThmZDNhOWZhXkEyXkFqcGdeQXVyMTE0MTY2Mzk2._V1_FMjpg_UX1000_.jpg',
+    'https://m.media-amazon.com/images/M/MV5BNzJiMTI3ZTMtNTFhYy00OWNhLWExYjItNzU4NmRkNzdmYmJkXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg',
+];
+
+export const TypeData = [
+    {
+        item1: [
+            {
+                i1: 'Other Providers',
+                i2: 'Prime Video',
+                i3: 'Prime Video(Rent/Buy)',
+                i4: 'In Theatres',
+            },
+        ],
+        item2: [
+            {
+                i1: 'Drama',
+                i2: 'Action',
+                i3: 'Thriller',
+                i4: 'More...',
+            },
+        ],
+    },
+];
+
+export const movieData = [
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BN2RjZDJhYzUtOTQ5Yy00OWM3LWE5OTctM2Y0YWVmNzAzODllXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg',
+        rating: 8.5,
+        title: 'Sita Rama',
+        year: 2022,
+        time: 165,
+    },
+    {
+        image: 'https://gutsyindians.com/wp-content/uploads/2022/03/FOw6RHHaQAEJopz-1-1170x1657.jpg',
+        rating: 8.0,
+        title: 'Rise Roar Revolt',
+        year: 2022,
+        time: 187,
+    },
+    {
+        image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSCmwbcVs6Qbwf_vSX04HN4uGW7IwamqXrb8o0HIVWvUbuTTLt2',
+        rating: 7.4,
+        title: 'Jogi',
+        year: 2022,
+        time: 80,
+    },
+    {
+        image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQEJ6XEfDIp0CAlNbhoICrQjgwfdN14rYu19GVnL3NroFfsimr7',
+        rating: 8.5,
+        title: 'Chup',
+        year: 2022,
+        time: 135,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw2VxIEE0vybIq_UaVr9SFv-906yzKYuuln0SF07dCB3frs2Qj',
+        rating: 7.4,
+        title: 'VR(Vikrant Rona)',
+        year: 2022,
+        time: 148,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BNmNmYjBmYjgtZjlmZC00OWQyLTgzZGQtMGVkNDQ5NDlkZTM2XkEyXkFqcGdeQXVyMTA5MjQ4MDg4._V1_.jpg',
+        rating: 8.4,
+        title: 'Vikram',
+        year: 2022,
+        time: 175,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BZjdjOTQwY2UtZDM5Zi00YTU0LWIzOTQtMTFjZThmZDNhOWZhXkEyXkFqcGdeQXVyMTE0MTY2Mzk2._V1_FMjpg_UX1000_.jpg',
+        rating: 6.9,
+        title: 'Hit: The First Case',
+        year: 2022,
+        time: 132,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BNzJiMTI3ZTMtNTFhYy00OWNhLWExYjItNzU4NmRkNzdmYmJkXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg',
+        rating: 8.0,
+        title: 'Vendhu Thanindhathu Kaadu',
+        year: 2022,
+        time: 177,
+    },
+];
+
+export const TVData = [
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BNTA2YzMyOWUtODAxNC00ZTI4LThiZWItMzMxYjYxOGFmMDUzXkEyXkFqcGdeQXVyMzE4NjMwMjc@._V1_.jpg',
+        rating: 7.4,
+        title: 'Dahan: Raakan Ka Rahasya',
+        year: 2022,
+        time: 23,
+        eps: 9,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BNWM1OGE2MmItZWUzZi00OGY5LThhMmQtZmJjZmJlYmQ5NTkwXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
+        rating: 7.3,
+        title: 'Jamtara: Sabka Number Ayega',
+        year: 2020,
+        time: 187,
+        eps: 12,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BYjQ2YmE4OWUtNDZhZi00ZGYxLTg4MzYtYWIwMWI1NDQ5ZDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg',
+        rating: 8.5,
+        title: 'Delhi Crime',
+        year: 2019,
+        time: 23,
+        eps: 12,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BZmVjMDAzZjMtNGNiNS00NzY3LTk2YjUtYjg0ZWVhYzU3N2ZlXkEyXkFqcGdeQXVyMTE3MzIyMDM5._V1_FMjpg_UX1000_.jpg',
+        rating: 8.0,
+        title: 'Criminal Justice: Adhura',
+        year: 2022,
+        time: 35,
+        eps: 8,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BMWNjOTRhZmYtZGVjYy00OTdhLWI2ODMtMWMxMjgxMDFiYTc0XkEyXkFqcGdeQXVyMTAyMTE1MDA1._V1_.jpg',
+        rating: 8.1,
+        title: 'Criminal Justice',
+        year: 2019,
+        time: 20,
+        eps: 10,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BNjdmYzBkNTItZTdhNi00MTI5LTlkNjMtNTYxYjczMmZjMjQzXkEyXkFqcGdeQXVyNDY4NjAxNTc@._V1_FMjpg_UX1000_.jpg',
+        rating: 9.2,
+        title: 'NCR Days',
+        year: 2022,
+        time: 50,
+        eps: 5,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BZjBkZTZjMzQtZDJhMS00MzE3LTg0MDctZjZhZDhhYjFlODZjXkEyXkFqcGdeQXVyOTIxMzQ0NzU@._V1_FMjpg_UX1000_.jpg',
+        rating: 7.9,
+        title: 'Maharani',
+        year: 2021,
+        time: 32,
+        eps: 20,
+    },
+    {
+        image: 'https://m.media-amazon.com/images/M/MV5BMzRjZWVmMzItNTdmYS00OWEzLTgyOGUtNThiNTU2ZThlYjY0XkEyXkFqcGdeQXVyMTIxMDk2NDE4._V1_FMjpg_UX1000_.jpg',
+        rating: 8.7,
+        title: 'The Family Man',
+        year: 2019,
+        time: 25,
+        eps: 20,
+    },
+];
