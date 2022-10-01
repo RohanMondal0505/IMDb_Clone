3b77ce278f56e8fa2d1209024cc1a83d6d58141f
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-10-02 04:06:34 +0530

    Added TopNews Section

--- src/components/home/TopNewsSection.js
+++ src/components/home/TopNewsSection.js
@@ -1,10 +1,18 @@
 import {useTheme} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import { Strings } from '../../assets/values/Strings';
+import {
+    Dimensions,
+    FlatList,
+    Image,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import {CustomFonts} from '../../assets/values/CustomFonts';
+import {Strings} from '../../assets/values/Strings';
 import TitleContainer from '../Custom/TitleContainer';
 
-const TopNewsSection = () => {
+const TopNewsSection = props => {
     const {colors} = useTheme();
 
     const styles = StyleSheet.create({
@@ -19,10 +27,94 @@ const TopNewsSection = () => {
     return (
         <View style={styles.Container}>
             <TitleContainer title={Strings.Top_News} />
+            <News data={props.data} colors={colors} />
         </View>
     );
 };
 
 export default TopNewsSection;
 
-const styles = StyleSheet.create({});
+const {width} = Dimensions.get('window');
+
+const News = ({data, colors}) => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                marginVertical: 20,
+            }}>
+            <FlatList
+                data={data}
+                horizontal
+                scrollEnabled={false}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(item, index) => `key-${index}`}
+                renderItem={({item, index}) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            width: width / 2 - 20,
+                            height: 130,
+                            marginRight: 20,
+                            flexDirection: 'row',
+                        }}>
+                        <View style={{flex: 1}}>
+                            <Text
+                                style={{
+                                    fontFamily: CustomFonts.Regular,
+                                    fontWeight: '600',
+                                    color: colors.textColor,
+                                    flex: 0.49,
+                                    lineHeight: 17,
+                                    fontSize: 17,
+                                    paddingRight: 10,
+                                }}>
+                                {item.title}
+                            </Text>
+                            <Text
+                                style={{
+                                    flex: 1,
+                                    marginVertical: 6,
+                                    fontSize: 14,
+                                    paddingRight: 10,
+                                    fontFamily: CustomFonts.Regular,
+                                    color: colors.SocialButtonColor,
+                                }}>
+                                {item.description}
+                            </Text>
+                            <View
+                                style={{
+                                    flexDirection: 'row',
+                                }}>
+                                <Text
+                                    style={{
+                                        fontSize: 12,
+                                        color: colors.SocialButtonColor,
+                                    }}>
+                                    {item.time} |{' '}
+                                </Text>
+                                <Text
+                                    style={{
+                                        fontSize: 12,
+                                        color: colors.SocialButtonColor,
+                                    }}>
+                                    {item.by.slice(0, 20)}..
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View style={{flex: 0.4}}>
+                            <Image
+                                source={{uri: item.image}}
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                }}
+                            />
+                        </View>
+                    </View>
+                )}
+            />
+        </View>
+    );
+};
