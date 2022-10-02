30e142bc63563c3eda67f32d07c53484e8dcbbdd
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-10-02 18:11:13 +0530

    Added New Strings

--- src/assets/values/Strings.js
+++ src/assets/values/Strings.js
@@ -10,7 +10,7 @@ export const Strings = {
     IMDb_Spotlight_Text:
         'Discover top-rated and trending movies, celebrity interviews and behind-the-scenes previews from the latest in Indian entertainment.',
     SEE_ALL: 'SEE ALL',
-    What_to_Watch_Text: 'What to Watch',
+    What_to_Watch: 'What to watch',
     What_to_Watch_Text:
         'Showa and movies available to watch from your WatchList',
     Top_Pics_for_you: 'Top Pics for you',
@@ -23,4 +23,9 @@ export const Strings = {
     Fan_Favorites_Text: "This week's top TV and Movies",
     Top_News: 'Top News',
     Born_Today: 'Born Today',
+    From_your_Watchlist:'From your Watchlist',
+    Your_Watchlist_Empty: 'Your Watchlist is Empty',
+    Your_Watchlist_Empty_Text: 'save shows and movies to keep track of what you want to watch.',
+    Browse_popular_movies:'Browse popular movies'
+
 };
