f24ced5457616dcf0e64a83f42e3f7bb62da3809
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-10-13 13:00:22 +0530

    Remove Loading Screen

--- App.js
+++ App.js
@@ -2,27 +2,27 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {CustomColors} from './src/assets/values/CustomColors';
 import {CustomFonts} from './src/assets/values/CustomFonts';
-import { Strings } from './src/assets/values/Strings';
+// import { Strings } from './src/assets/values/Strings';
 import RootNavigation from './src/navigation/RootNavigation';
 
 const App = () => {
-    const [isLoading, setIsLoading] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false)
-        }, 1000);
-    }, []);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setIsLoading(false)
+    //     }, 1000);
+    // }, []);
 
     return (
         <>
-            {isLoading ? (
+            {/* {isLoading ? (
                 <View style={styles.Splash}>
                     <Text style={styles.Text}>{Strings.app_Name}</Text>
                 </View>
-            ) : (
+            ) : ( */}
                 <RootNavigation />
-            )}
+            {/* )} */}
         </>
     );
 };
