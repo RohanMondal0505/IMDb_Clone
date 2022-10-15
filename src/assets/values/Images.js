bb32e759c5a02d3931a61f555c678ee4049eb1c2
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-10-15 15:29:19 +0530

    Added 2 Images

--- src/assets/values/Images.js
+++ src/assets/values/Images.js
@@ -1,3 +1,5 @@
 export const Images = {
-    IMDb_App_Icon:require('../images/IMDb_App_Icon.png'),
+    IMDb_App_Icon: require('../images/IMDb_App_Icon.png'),
+    google: require('../images/google.png'),
+    facebook: require('../images/facebook.png')
 }
