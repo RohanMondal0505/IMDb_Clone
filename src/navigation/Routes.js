b32a3bdc89056c3c9a070f49c8a2ca84b2fb3eec
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-10-15 21:05:15 +0530

    Added AsyncStorage

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,16 +1,18 @@
+import auth from '@react-native-firebase/auth';
 import {
     DarkTheme,
     DefaultTheme,
     NavigationContainer,
 } from '@react-navigation/native';
+import {useEffect, useState} from 'react';
 import {useColorScheme} from 'react-native';
 import {
     CustomDarkColors,
     CustomLightColors,
 } from '../assets/values/CustomColors';
 import {GlobalVariables} from '../Context/AppContext';
-import AuthStack from './AuthStack';
 import AppStack from './AppStack';
+import AuthStack from './AuthStack';
 
 const customDarkTheme = {
     ...DarkTheme,
@@ -30,7 +32,22 @@ const customLightTheme = {
 
 const Routes = () => {
     const colorScheme = useColorScheme();
-    const {user} = GlobalVariables();
+    const {user, setUser, setUserDataInAsyncStorage} = GlobalVariables();
+    const [initializing, setInitializing] = useState(true);
+
+    const onAuthStateChanged = userValue => {
+        setUser(userValue);
+        setUserDataInAsyncStorage(userValue);
+        if (initializing) setInitializing(false);
+    };
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+
+    if (initializing) return null;
+
     return (
         <NavigationContainer
             theme={colorScheme === 'dark' ? customDarkTheme : customLightTheme}>
