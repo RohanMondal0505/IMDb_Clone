2ac2c9cd31339ef463bf33e22011c62fe5560e3f
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-10-17 10:25:48 +0530

    Remove Splash Screen

--- App.js
+++ App.js
@@ -1,44 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {CustomColors} from './src/assets/values/CustomColors';
-import {CustomFonts} from './src/assets/values/CustomFonts';
-// import { Strings } from './src/assets/values/Strings';
-import RootNavigation from './src/navigation/RootNavigation';
+import React from 'react';
+import {AppProvider} from './src/Context/AppContext';
+import Routes from './src/navigation/Routes';
 
 const App = () => {
-    // const [isLoading, setIsLoading] = useState(false);
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setIsLoading(false)
-    //     }, 1000);
-    // }, []);
-
     return (
         <>
-            {/* {isLoading ? (
-                <View style={styles.Splash}>
-                    <Text style={styles.Text}>{Strings.app_Name}</Text>
-                </View>
-            ) : ( */}
-                <RootNavigation />
-            {/* )} */}
+            <AppProvider>
+                <Routes />
+            </AppProvider>
         </>
     );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-    Splash: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: CustomColors.primaryColor,
-    },
-    Text: {
-        fontSize: 200,
-        fontFamily: CustomFonts.Bold,
-        color: CustomColors.black,
-    },
-});
