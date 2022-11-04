a8e25bbc77f4bda47e40aea926d62e4471422aeb
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-11-04 21:59:17 +0530

    Color Change

--- src/components/Custom/Loader.js
+++ src/components/Custom/Loader.js
@@ -1,20 +1,21 @@
+import { useTheme } from '@react-navigation/native';
 import React from 'react';
 import {ActivityIndicator, Text, View} from 'react-native';
-import { CustomColors } from '../../assets/values/CustomColors';
 
 const Loader = () => {
+    const { colors } = useTheme();
     return (
         <View
             style={{
                 position: 'absolute',
-                backgroundColor: 'rgba(0,0,0,0.5)',
+                backgroundColor: colors.mainBackgroundColor,
                 height: '100%',
                 width: '100%',
                 zIndex: 10,
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-            <ActivityIndicator size={100} color={CustomColors.primaryColor} />
+            <ActivityIndicator size={100} color={colors.primaryColor} />
         </View>
     );
 };
