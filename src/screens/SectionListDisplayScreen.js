becae63464e56e2cc4a174e021c5625e1d915963
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-11-18 20:38:18 +0530

    Screen Created

---
+++ src/screens/SectionListDisplayScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const SectionListDisplayScreen = () => {
+    return (
+        <View>
+            <Text>SectionListDisplayScreen</Text>
+        </View>
+    );
+};
+
+export default SectionListDisplayScreen;
+
+const styles = StyleSheet.create({});
