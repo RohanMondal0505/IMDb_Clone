73fb8450bc3c0396dd12431fa24decccd7a84d07
RohanMondal0505 <rohanmondal1438@gmail.com>
2022-11-26 13:44:50 +0530

    Modified This Screen

--- src/screens/AutStack/OnBoardingScreen.js
+++ src/screens/AutStack/OnBoardingScreen.js
@@ -5,7 +5,7 @@ import Onboarding from 'react-native-onboarding-swiper';
 const OnBoardingScreen = ({navigation}) => {
     const Done = ({...props}) => (
         <TouchableOpacity {...props} style={{marginHorizontal: 20}}>
-            <Text style={{fontSize:16,fontWeight:'500'}}>Done</Text>
+            <Text style={{fontSize: 16, fontWeight: '500'}}>Done</Text>
         </TouchableOpacity>
     );
 
@@ -16,26 +16,35 @@ const OnBoardingScreen = ({navigation}) => {
             onDone={() => navigation.navigate('LoginRoutes')}
             pages={[
                 {
-                    backgroundColor: '#ff0',
+                    backgroundColor: '#F5C518',
                     image: (
                         <Image
                             source={require('../../assets/images/IMDb_App_Icon.png')}
                             style={{height: 200, width: 200}}
                         />
                     ),
-                    title: 'Onboarding 1',
-                    subtitle: 'React Native',
+                    title: (
+                        <Text
+                            style={{
+                                fontSize: 30,
+                                fontWeight: '600',
+                                color: 'rgba(0,0,0,0.8)',
+                            }}>
+                            Welcome
+                        </Text>
+                    ),
+                    subtitle: 'Hope you like it',
                 },
                 {
-                    backgroundColor: '#fff',
+                    backgroundColor: '#F5C518',
                     image: (
                         <Image
                             source={require('../../assets/images/IMDb_App_Icon.png')}
                             style={{height: 200, width: 200}}
                         />
                     ),
-                    title: 'Onboarding 2',
-                    subtitle: 'Done with React Native Onboarding Swiper',
+                    title: 'Movie Rating App',
+                    subtitle: 'You can see all movie rating all over the world',
                 },
             ]}
         />
